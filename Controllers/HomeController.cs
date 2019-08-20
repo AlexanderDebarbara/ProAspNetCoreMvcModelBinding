a5dfec0e8248557656eb450d1fc0f27d909d6c77
Alexander <debarbara4@gmail.com>
2019-08-20 19:49:45 -0400

    create controller Home

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAspNetCoreMvcModelBinding.Repository;
+
+namespace ProAspNetCoreMvcModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public ViewResult Index(int id)
+        {
+            return View("Index", repository[id]);
+        }
+    }
+}
